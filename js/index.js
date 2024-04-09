0a41201bde04e6ba71d62a7f46bd5a20366910b6
mawreh <mawreh1714@gmail.com>
2024-04-08 21:08:37 -0400

    added carousel

--- js/index.js
+++ js/index.js
@@ -1,23 +1,39 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+  // get buttons
+  const leftBtn = document.querySelector('.left');
+  const rightBtn = document.querySelector('.right');
 
-// image carousel
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-const slides = document.querySelector('.images');
+  // get carousel items
+  const carouselItems = Array.from(document.querySelectorAll('.carousel-item'));
+  const CAROUSEL_SIZE = carouselItems.length;
 
-let index = 0;
+  // keep track of which images are currently selected
+  // using active class
+  leftBtn.addEventListener('click', swipe);
+  rightBtn.addEventListener('click', swipe);
 
-prevBtn.addEventListener('click', () => {
-  index = (index - 1 + slides.children.length) % slides.children.length;
-  updateSlidePosition();
+  // swipe function takes (e) as the event argument
+  function swipe(e) {
+    const currentCarouselItem = document.querySelector('.carousel-item.active');
+    const currentIndex = carouselItems.indexOf(currentCarouselItem);
+    let nextIndex;
+
+    if (e.currentTarget.classList.contains('left')) {
+      if (currentIndex === 0) {
+        nextIndex = CAROUSEL_SIZE - 1;
+      } else {
+        nextIndex = currentIndex - 1;
+      }
+    } else {
+      if (currentIndex === CAROUSEL_SIZE - 1) {
+        nextIndex = 0;
+      } else {
+        nextIndex = currentIndex + 1;
+      }
+    }
+
+    carouselItems[nextIndex].classList.add('active');
+    currentCarouselItem.classList.remove('active');
+    carouselItems[currentIndex].classList.remove('active');
+  }
 });
-
-nextBtn.addEventListener('click', () => {
-  index = (index + 1) % slides.children.length;
-  updateSlidePosition();
-});
-
-function updateSlidePosition() {
-  const slideWidth = slides.children[0].offsetWidth;
-  slides.style.transform = `translateX(${-index * slideWidth}px)`;
-}
